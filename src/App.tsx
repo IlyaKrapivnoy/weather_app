2e4e3cd0c402feb9215d92669e0598484f9696c8
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-02-19 20:26:52 +0100

    upd project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { FC } from 'react';
-import './App.css';
 
-const App: FC = () => {
-    return <div className='App'></div>;
-};
+const App: FC = () => <div className='App'></div>;
+
 
 export default App;
